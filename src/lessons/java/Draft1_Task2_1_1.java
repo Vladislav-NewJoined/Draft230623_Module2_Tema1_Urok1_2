a517cda7d637225cbd094f9798ce9a144f7c4b2d
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-06-25 00:03:55 +0300

    Initial commit

--- src/lessons/java/Draft1_Task2_1_1.java
+++ src/lessons/java/Draft1_Task2_1_1.java
@@ -3,6 +3,8 @@ package lessons.java;
 //  Draft1_Task2_1_1    Задание_TaskМодуль - Тема - Урок
 //  здесь пример: https://stackoverflow.com/questions/43500275/test-interface-implementation-for-simple-calculator
 
+
+//        Пример _ ППППППППППППППППППППППППППППППППППП
 interface Operation {
     void calculate(int a, int b);
 
@@ -32,3 +34,51 @@ public class Draft1_Task2_1_1 {
         add.calculate();
     }
 }
+//        Конец Примера _ КККККККККККККККК
+
+
+
+
+
+
+
+
+////        Пример 1 ППППППППППППППППППППППППППППППППППП
+//interface Operation {
+//    void calculate(int a, int b);
+//
+//    void calculate();
+//}
+//public class Draft1_Task2_1_1 {
+//    static int a = 10;
+//    static int b = 2;
+//    static int result = 0;
+//    public static void main(String[] args) {
+//        System.out.println("Задание : \n1.\tЗадание в рамках интерфейса java нужно создать калькулятор " +
+//                "\nвыполняющий 4 основных арифметических функций, сложение, вычитание, деление, умножение, " +
+//                "\nа также осуществить вывод на результатов на экран\n\nРешение: ");
+//        Operation add = new Operation() {
+//
+//            @Override
+//            public void calculate(int a, int b) {
+//                result = a + b;
+//                System.out.println("Сложение: " + a + " + " + b + " = " + result);
+//            }
+//
+//            @Override
+//            public void calculate() {
+//
+//            }
+//        };
+//        add.calculate();
+//    }
+//}
+////        Конец Примера 1 КККККККККККККККК
+
+
+
+
+
+
+
+
